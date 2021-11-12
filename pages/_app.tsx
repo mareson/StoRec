6fe85f0eebcf3f9dc943417290dac7be4e24b48a
Ondřej Mareš <ondramares@ondramares.com>
2021-11-12 23:47:01 +0100

    Material ui integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {CacheProvider, EmotionCache} from "@emotion/react";
+import createEmotionCache from "../services/createEmotionCache";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import theme from "../props/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { emotionCache = clientSideEmotionCache, Component, pageProps } = props;
+
+  return (
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+  );
 }
 
 export default MyApp
